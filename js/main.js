6123add7b5bc80f9195f777df0e75cb219b1aae1
laurajacobs <laurajacobs@talktalk.net>
2017-04-22 18:31:05 +0100

    adding elements to home page and linking an about me page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-  $('h1').click(function(){
-    $(this).hide();
-  })
+  $('li').mouseover(function(){
+    $(this).fadeTo('fast',0.5);
+  });
+  $('li').mouseleave(function(){
+    $(this).fadeTo('fast',1);
+  });
+});
+
+$(document).ready(function(){
+  $('img').mouseover(function(){
+    $(this).fadeTo('fast',0.5);
+  });
+  $('img').mouseleave(function(){
+    $(this).fadeTo('fast',1);
+  });
+
 });
